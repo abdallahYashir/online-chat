3cfdf371ec3edc1c86f71c215ff00377fc4cde92
Abdallah Yashir <abdallah.yashir@chb-technologies.ch>
2016-09-14 09:26:20 +0400

    Added sidebar image with profile picture.

--- app/online_chat_controller.js
+++ app/online_chat_controller.js
@@ -9,12 +9,12 @@
             var vm = this;
 
             vm.chats = [
-                {name: 'Sternritter B', comment: 'Hey guys sup?', image: 'assets/images/01.jpeg', time: '1:22 PM'},
-                {name: 'Ywach', comment: 'looks good', image: 'assets/images/02.jpeg', time: '2:17 PM'},
-                {name: 'Aizen', comment: 'Nice looking', image: 'assets/images/03.jpg', time: '2:34 PM'},
-                {name: 'Tensa', comment: 'neat!', image: 'assets/images/04.jpg', time: '2:35 PM'},
-                {name: 'White', comment: 'cool!', image: 'assets/images/05.jpg', time: '3:12 PM'},
-                {name: 'Bazz', comment: 'Top, angular material stuff', image: 'assets/images/06.jpeg', time: '4:00 PM'},
+                {name: 'Darrel Royston', comment: 'Hey guys sup?', image: 'assets/images/01.jpeg', time: '1:22 PM'},
+                {name: 'Dominica Oelke', comment: 'looks good', image: 'assets/images/02.jpeg', time: '2:17 PM'},
+                {name: 'Sabra Vassell', comment: 'Nice looking', image: 'assets/images/03.jpg', time: '2:34 PM'},
+                {name: 'Vincenzo Messina', comment: 'neat!', image: 'assets/images/04.jpg', time: '2:35 PM'},
+                {name: 'Lilly Sallis', comment: 'cool!', image: 'assets/images/05.jpg', time: '3:12 PM'},
+                {name: 'Austin Brocious', comment: 'Top, angular material stuff', image: 'assets/images/06.jpeg', time: '4:00 PM'},
             ];
 
         }
