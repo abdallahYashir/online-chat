31435eb8c425a98aa19bb8e39722082efa2766c1
Abdallah Yashir <abdallah.yashir@chb-technologies.ch>
2016-09-15 09:49:35 +0400

    Added socket events in controller

--- app/online_chat_controller.js
+++ app/online_chat_controller.js
@@ -25,6 +25,124 @@
             }
         });
 
+        // Declare socket io
+        var socket = io();
+
+        // Chat name
+        vm.username = '';
+
+        // Typed message
+        vm.message = '';
+
+        var chatName = 'Enter User Name...';
+        var typeMessage = 'Type Message...';
+
+        var typing = false;
+        var timeout = null;
+
+        vm.hasUserName = false;
+        vm.placeholder = chatName;
+
+        // Store username in local storage
+        if (localStorage.getItem('chat.username')) {
+            vm.username = localStorage.getItem('chat.username');
+            vm.hasUserName = true;
+            vm.placeholder = typeMessage;
+        }
+
+        // On submit
+        vm.submit = function() {
+
+            // if first time, enter chatName
+            if (!vm.hasUserName) {
+
+                vm.hasUserName = true;
+                vm.username = vm.message;
+
+                vm.placeholder = typeMessage;
+                localStorage.setItem('chat.username', vm.username);
+
+                socket.emit('connected', vm.username);
+            } else if (vm.hasUserName) {
+                socket.emit('chat message', {
+                    username: vm.username,
+                    message: vm.message
+                });
+            }
+
+            vm.message = '';
+            return false;
+        }; // end vm.submit
+
+        // When received message
+        socket.on('chat message', function(msg) {
+            // console.log('received emit');
+            // $('#messages').append($('<li>').text(msg));
+        });
+
+        // Detect when tab/window is going to close
+        window.onbeforeunload = function() {
+            socket.emit('disconnect', { username: vm.username });
+        };
+
+        // Received all previous messages in the group chat
+        socket.on('messages', function(msg) {
+            // if message is not empty and user has entered user name
+            if (msg !== null && msg !== '') {
+
+                // Parse string to array
+                msg = JSON.parse(msg);
+
+                if (msg.length > 0) {
+                    msg.forEach(function(message) {
+                        // $('#messages').append($('<li>').text(message));
+                    });
+                }
+            }
+        }); // end socket.on messages
+
+        // Detect user typing on and shows
+        // show typing no longer 
+        function timeoutFunction() {
+            typing = false;
+            socket.emit('typingStop', { username: vm.username });
+        }
+
+        // Emits typing whenever user types a key
+        vm.onKeyDownNotEnter = function(keyEvent) {
+            // if Enter key is pressed, launch timeout directly
+            if (keyEvent.keyCode === 13) {
+                timeoutFunction();
+                // $('.' + $scope.username).remove();
+                typing = false;
+                return;
+            }
+            // if typing false
+            if (!typing) {
+                typing = true;
+                socket.emit('typing', { username: vm.username });
+                timeout = setTimeout(timeoutFunction, 5000);
+            } else {
+                clearTimeout(timeout);
+                timeout = setTimeout(timeoutFunction, 5000);
+            }
+        };
+
+        // On user typing
+        socket.on('is typing', function(msg) {
+            if (msg !== '' && msg !== vm.username) {
+                // $('#whoIsTyping').append($('<li class="' + msg + '">').text(msg + ' is typing...'));
+            }
+
+        });
+
+        // On user stop typing
+        socket.on('stop typing', function(msg) {
+            try {
+                // $('.' + msg).remove();
+            } catch (exception) {}
+        });
+
     } // end function ChatController
 
 })();
