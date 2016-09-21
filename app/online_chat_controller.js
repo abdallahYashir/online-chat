c2d2ad2a092fdac3a79f069552c709559b937f0d
Abdallah Yashir <abdallah.yashir@chb-technologies.ch>
2016-09-21 12:38:20 +0400

    Updated sending of chat message. Still a few bugs remaining.

--- app/online_chat_controller.js
+++ app/online_chat_controller.js
@@ -9,12 +9,12 @@
         var vm = this;
 
         vm.chats = [
-            { name: 'Darrel Royston', comment: 'Hey guys sup?', image: 'assets/images/01.jpeg', time: '1:22 PM', color: '#6688AA', online: true },
-            { name: 'Dominica Oelke', comment: 'looks good', image: 'assets/images/02.jpeg', time: '2:17 PM', color: '#3E6677', online: false },
-            { name: 'Sabra Vassell', comment: 'Nice looking', image: 'assets/images/03.jpg', time: '2:34 PM', color: '#879996', online: false },
-            { name: 'Vincenzo Messina', comment: 'neat!', image: 'assets/images/04.jpg', time: '2:35 PM', color: '#FFDDBB', online: true },
-            { name: 'Lilly Sallis', comment: 'cool!', image: 'assets/images/05.jpg', time: '3:12 PM', color: '#5D9647', online: false },
-            { name: 'Austin Brocious', comment: 'Top, angular material stuff', image: 'assets/images/06.jpeg', time: '4:00 PM', color: '#CC4ECC', online: true },
+            { username: 'Darrel Royston', message: 'Hey guys sup?', image: 'assets/images/01.jpeg', time: '1:22 PM', color: '#6688AA', online: true },
+            { username: 'Dominica Oelke', message: 'looks good', image: 'assets/images/02.jpeg', time: '2:17 PM', color: '#3E6677', online: false },
+            { username: 'Sabra Vassell', message: 'Nice looking', image: 'assets/images/03.jpg', time: '2:34 PM', color: '#879996', online: false },
+            { username: 'Vincenzo Messina', message: 'neat!', image: 'assets/images/04.jpg', time: '2:35 PM', color: '#FFDDBB', online: true },
+            { username: 'Lilly Sallis', message: 'cool!', image: 'assets/images/05.jpg', time: '3:12 PM', color: '#5D9647', online: false },
+            { username: 'Austin Brocious', message: 'Top, angular material stuff', image: 'assets/images/06.jpeg', time: '4:00 PM', color: '#CC4ECC', online: true },
         ];
 
         // Use of random images and colors
@@ -115,7 +115,11 @@
             } else if (vm.hasUserName) {
                 socket.emit('chat message', {
                     username: vm.username,
-                    message: vm.message
+                    message: vm.message,
+                    time: _.now(),
+                    image: vm.connectedClient.image,
+                    color: vm.connectedClient.color,
+                    online: true
                 });
             } // end if
 
@@ -137,6 +141,9 @@
         socket.on('chat message', function(msg) {
             // console.log('received emit');
             // $('#messages').append($('<li>').text(msg));
+            console.log('chat message - msg: ', msg);
+            vm.chats.push(msg);
+            $scope.$apply('vm.chats');
         });
 
         // Detect when tab/window is going to close
