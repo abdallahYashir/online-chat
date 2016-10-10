170fa4e598c726dbc5dd41d254dd8d262f761d36
Abdallah Yashir <abdallah.yashir@chb-technologies.ch>
2016-10-10 09:59:30 +0400

    Updated package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 // included packages
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
-var coffee = require('gulp-coffeescript-concat');
+var coffee = require('gulp-coffee');
+
+var paths = {
+    scripts: 'app/streamUIApp/*.coffee',
+    styles: 'app/streamUIApp/*.styl'
+}
 
 gulp.task('stylus', function(){
     return gulp.src('app/**/*.styl')
@@ -11,4 +16,9 @@ gulp.task('stylus', function(){
 gulp.task('coffee', function(){
     return gulp.src('app/**/*.coffee')
     .pipe(gulp.dest('app/streamUIApp/'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch(paths.scripts, ['coffee']);
+    gulp.watch(paths.styles, ['stylus'])
 });
